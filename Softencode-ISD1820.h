965e65a3dd36fdcd61a2dd95279222ecc8d1f681
MapMaths <62785981+MapMaths@users.noreply.github.com>
2020-08-21 16:35:27 +0800

    Update Softencode-ISD1820.h

--- Softencode-ISD1820.h
+++ Softencode-ISD1820.h
@@ -1,1 +1,17 @@
+/*
+  Softencode-ISD1820.h - Library for chip ISD1820.
+  Created by Softencode Org., August 21, 2020.
+  Released into the public domain.
+*/
 
+#ifndef Softencode-ISD1820_h
+#define Softencode-ISD1820_h
+
+#include <Arduino.h>
+
+class Softencode-ISD1820 {
+  public:
+    void setPin(int REC, int PLAY_E, int PLAY_L， int MIC, int MICREF, int AGC, int SPa, int SPb);
+}
+
+#endif
